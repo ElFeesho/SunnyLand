71ae629dde05d2dd162334a65f5131947c524baf
Chris Sawczuk <chris.sawczuk@bbc.co.uk>
2017-11-18 17:08:30 +0000

    Extract camera

--- game/app/main.cpp
+++ game/app/main.cpp
@@ -94,6 +94,49 @@ void Player::fall() {
     _state = State::Fall;
 }
 
+class Camera {
+public:
+
+    void position(double x, double y);
+
+    void target(double x, double y);
+
+    void pan();
+
+    int32_t x();
+    int32_t y();
+
+private:
+    double _camX{0};
+    double _camY{0};
+
+    double _camTargetX{0};
+    double _camTargetY{0};
+};
+
+void Camera::position(double x, double y) {
+    _camX = x;
+    _camY = y;
+}
+
+void Camera::target(double x, double y) {
+    _camTargetX = x;
+    _camTargetY = y;
+}
+
+void Camera::pan() {
+    _camX += (_camTargetX - _camX) / 8.0;
+    _camY += (_camTargetY - _camY) / 8.0;
+}
+
+int32_t Camera::x() {
+    return static_cast<int32_t>(_camX);
+}
+
+int32_t Camera::y() {
+    return static_cast<int32_t>(_camY);
+}
+
 class MainMenuScene : public SL::Scene {
 public:
     explicit MainMenuScene(SL::Engine &engine) :
@@ -102,11 +145,9 @@ public:
             _mg{_engine.createParallax("../Resources/island-middleground.png", 1.0f)},
             _map{_engine.createMap(readFile("../Resources/first.json"), "../Resources/forest-tileset.png")},
             _player{SL::JSONSpriteFactory{engine}.parse(readFile("../Resources/rabbit.json"))} {
-        _camX = _map.cameraSpawnX();
-        _camY = _map.cameraSpawnY() - 100;
 
-        _camTargetX = _map.playerSpawnX();
-        _camTargetY = _map.playerSpawnY() - 100;
+        _camera.position(_map.cameraSpawnX(), _map.cameraSpawnY());
+        _camera.target(_map.playerSpawnX(), _map.playerSpawnY()-100);
 
         _playerX = _map.playerSpawnX();
         _playerY = _map.playerSpawnY();
@@ -115,33 +156,41 @@ public:
     void keyEvent(SL::KeyType key, SL::ActionType action) override {
         if (action == SL::ActionType::Press) {
             if (key == SL::KeyType::Right) {
-                _player.walk();
-                _player.lookRight();
+                if (_playerYSpeed > -0.1 && _playerYSpeed < 0.1) {
+                    _player.walk();
+                    _player.lookRight();
+                }
                 _playerXSpeed = 2;
             } else if (key == SL::KeyType::Left) {
-                _player.walk();
-                _player.lookLeft();
+                if (_playerYSpeed > -0.1 && _playerYSpeed < 0.1) {
+                    _player.walk();
+                    _player.lookLeft();
+                }
                 _playerXSpeed = -2;
             } else if (key == SL::KeyType::Jump) {
-                _playerYSpeed = -5.0;
-                _player.jump();
+                if (_playerYSpeed > -0.1 && _playerYSpeed < 0.1) {
+                    _playerYSpeed = -5.0;
+                    _player.jump();
+                }
             }
         } else {
             if (key == SL::KeyType::Right || key == SL::KeyType::Left) {
-                _player.idle();
+                if (_playerYSpeed > -0.1 && _playerYSpeed < 0.1) {
+                    _player.idle();
+                }
                 _playerXSpeed = 0;
             }
         }
     }
 
     void update(long delta) override {
-        _bg.scroll(static_cast<int32_t>(-_camX), static_cast<int32_t>(-_camY));
-        _mg.scroll(static_cast<int32_t>(-_camX), static_cast<int32_t>(-_camY));
+        _bg.scroll(static_cast<int32_t>(-_camera.x()), static_cast<int32_t>(-_camera.y()));
+        _mg.scroll(static_cast<int32_t>(-_camera.x()), static_cast<int32_t>(-_camera.y()));
         _bg.draw();
         _mg.draw();
 
-        _map.layer(0).draw(static_cast<int32_t>(-_camX), static_cast<int32_t>(-_camY));
-        _map.layer(1).draw(static_cast<int32_t>(-_camX), static_cast<int32_t>(-_camY));
+        _map.layer(0).draw(static_cast<int32_t>(-_camera.x()), static_cast<int32_t>(-_camera.y()));
+        _map.layer(1).draw(static_cast<int32_t>(-_camera.x()), static_cast<int32_t>(-_camera.y()));
 
         _playerX += _playerXSpeed;
         _playerY += _playerYSpeed;
@@ -163,12 +212,11 @@ public:
             _playerYSpeed = 0;
         }
 
-        _camTargetX = _playerX - 100;
-        _camTargetY = _playerY - 100;
-        _player.draw(delta, static_cast<int>(_playerX - _camX), static_cast<int>(_playerY - _camY));
+        _camera.target(_playerX - 100, _playerY - 100);
 
-        _camX += (_camTargetX - _camX) / 8.0;
-        _camY += (_camTargetY - _camY) / 8.0;
+        _player.draw(delta, static_cast<int>(_playerX - _camera.x()), static_cast<int>(_playerY - _camera.y()));
+
+        _camera.pan();
 
     }
 
@@ -186,11 +234,7 @@ private:
     double _playerXSpeed{0};
     double _playerYSpeed{0};
 
-    double _camX{0};
-    double _camY{0};
-
-    double _camTargetX{0};
-    double _camTargetY{0};
+    Camera _camera{};
 };
 
 
@@ -210,9 +254,7 @@ int main(int argc, char **argv) {
 
     engine.displayScene(&mainMenuScene);
 
-    while (engine.update()) {
-
-    }
+    while (engine.update());
 
     return 0;
 }
